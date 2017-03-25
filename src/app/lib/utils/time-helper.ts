eb082380266cbafa6fb106653d3c70072e760d8b
Alexander Bachmann <kontakt@alexanderbachmann.de>
2017-03-25 11:08:37 +0100

    fix(utils): Wrong reference in TimeHelper

--- src/app/lib/utils/time-helper.ts
+++ src/app/lib/utils/time-helper.ts
@@ -6,7 +6,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-import { StringHelper } from './StringHelper';
+import { StringHelper } from './string-helper';
 
 export class TimeHelper {
 	static formatDurationStringFromSeconds(seconds, formats?: {
