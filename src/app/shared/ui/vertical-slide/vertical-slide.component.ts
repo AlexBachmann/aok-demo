6314a59d691caa1ff619e3097f75ed30bbb3533c
Alexander Bachmann <kontakt@alexanderbachmann.de>
2017-04-11 10:26:21 +0200

    move to new animations package

--- src/app/shared/ui/vertical-slide/vertical-slide.component.ts
+++ src/app/shared/ui/vertical-slide/vertical-slide.component.ts
@@ -6,32 +6,32 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-import { Component, Input, OnInit } from '@angular/core';
-import {
-  trigger,
-  state,
-  style,
-  animate,
-  transition
-} from '@angular/animations';
+ import { Component, Input, OnInit } from '@angular/core';
+ import {
+ 	trigger,
+ 	state,
+ 	style,
+ 	animate,
+ 	transition
+ } from '@angular/animations';
 
-@Component({
-	selector: 'tekkl-vertical-slide',
-	templateUrl: './vertical-slide.component.html',
-	styleUrls: ['./vertical-slide.component.sass'],
-	animations: [
-		trigger('open', [
-			state('true' , style({ height: '*', padding: '*', display: '*' })),
-			state('false', style({ height: 0, padding: 0, display: 'none' })),
-			transition('void <=> *', animate('0ms')),
-			transition('* => *', animate('300ms ease-out')),
-	    ])
-    ],
-})
-export class VerticalSlideComponent implements OnInit {
-	@Input() open = false;
-	constructor() { }
+ @Component({
+ 	selector: 'tekkl-vertical-slide',
+ 	templateUrl: './vertical-slide.component.html',
+ 	styleUrls: ['./vertical-slide.component.sass'],
+ 	animations: [
+ 	trigger('open', [
+ 		state('true' , style({ height: '*', padding: '*', display: '*' })),
+ 		state('false', style({ height: 0, padding: 0, display: 'none' })),
+ 		transition('void <=> *', animate('0ms')),
+ 		transition('* => *', animate('300ms ease-out')),
+ 		])
+ 	],
+ })
+ export class VerticalSlideComponent implements OnInit {
+ 	@Input() open = false;
+ 	constructor() { }
 
-	ngOnInit() {
-	}
-}
+ 	ngOnInit() {
+ 	}
+ }
