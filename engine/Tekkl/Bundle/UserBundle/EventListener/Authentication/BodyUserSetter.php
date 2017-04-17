75af21b8f61a140cb0988f010f1cf5de9f6e4e4e
Alexander Bachmann <kontakt@alexanderbachmann.de>
2017-04-17 18:26:14 +0200

    add body setter authentication event listener

---
+++ engine/Tekkl/Bundle/UserBundle/EventListener/Authentication/BodyUserSetter.php
@@ -0,0 +1,22 @@
+<?php 
+/**
+ * This file is part of the TEKKL core package
+ *
+ * (c) Alexander Bachmann <email.bachmann@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Tekkl\Bundle\UserBundle\EventListener\Authentication;
+
+use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationSuccessEvent;
+
+class BodyTokenRemover {
+	public function onAuthenticationSuccess(AuthenticationSuccessEvent $event){
+		$data = $event->getData();
+		$user = $event->getUser();
+		$userData = $user->getPublicData();
+		$data['user'] = $userData;
+		$event->setData($data);
+	}
+}
