795877e7e3d17fbb7179e043cb7fe66cfff0534f
Alexander Bachmann <kontakt@alexanderbachmann.de>
2017-04-17 18:24:33 +0200

    Add User Component to Backend

---
+++ src/app/shared/authentication/config.ts
@@ -0,0 +1,20 @@
+/**
+ * This file is part of the TEKKL core package
+ *
+ * (c) Alexander Bachmann <email.bachmann@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+export default {
+	loginUrl : {
+		frontend: '/user/login',
+		backend: '/login'
+	},
+	userStorage: {
+		localStorage: {
+			key: 'tekkl-user'
+		}
+	}
+}
