b6b9c391406190da766267f67faff997fb672acf
Alexander Bachmann <kontakt@alexanderbachmann.de>
2017-07-02 12:13:07 +0200

    Add OffCanvas State to apps root elemeent

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -8,11 +8,22 @@
  */
 import { Component } from '@angular/core';
 import { Registry } from './lib/utils/registry';
+import { Observable } from 'rxjs/Observable';
+import { Store } from '@ngrx/store';
+import { AppState } from './shared/state/app.state';
 
 @Component({
-	selector: 'app-root',
-	templateUrl: './app.component.html'
+	selector: '[app-root]',
+	templateUrl: './app.component.html',
+	host: {
+		'[class.offcanvas-open]' : 'offCanvasState' 
+	}
 })
 export class AppComponent {
-	title = 'app works!'
+	offCanvasState: boolean;
+	constructor(private store: Store<AppState>){
+		this.store.select('offcanvas').subscribe((state: boolean) => {
+			this.offCanvasState = state;
+		});
+	}
 }
